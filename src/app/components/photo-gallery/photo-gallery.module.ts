1d8c2fd40cc9c97754e1209f753106ddd57917ef
Michael Yuen <michael.yuen@ey.com>
2017-07-05 22:55:09 -0400

    Implemented simple full-screen grid

--- src/app/components/photo-gallery/photo-gallery.module.ts
+++ src/app/components/photo-gallery/photo-gallery.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { MasonryModule } from 'angular2-masonry';
 import { PhotoGalleryComponent } from './index';
 
 @NgModule({
+	imports: [
+		MasonryModule,
+		SharedModule
+	],
 	declarations: [PhotoGalleryComponent],
 	exports: [PhotoGalleryComponent]
 })
